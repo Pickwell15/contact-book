bfbf97f2b2a45ced2441b40179d739c8ed0309e8
Joshua Pickwell <pickwelljoshua@gmail.com>
2021-08-28 15:24:32 +0100

    updates
    - Made some functions asynchronous:
        'Main._save()'
        'Main._read_json()'
    
    - Added DocStrings and comments
    
    - Updated .gitignore

--- contact.py
+++ contact.py
@@ -1,21 +1,64 @@
 class Contact:
+    """
+    ATTRIBUTES
+    ---------------
+    PRIVATE | __NAME_DEFAULT : str
+    PRIVATE | __NUM_DEFAULT : str
+    PRIVATE | __ADDR_DEFAULT : str
+
+    METHODS
+    ---------------
+    PRIVATE | __init__ -> None
+    PRIVATE | __del__ -> None
+    PRIVATE | __str__ -> str
+    PRIVATE | __repr__ -> str
+    """
 
     __NAME_DEFAULT: str = "UNKNOWN"
     __NUM_DEFAULT: str = "UNKNOWN"
     __ADDR_DEFAULT: str = "UNKNOWN"
 
-    def __init__(self, name: str = self.__NAME_DEFAULT, num: str = self.__NUM_DEFAULT, addr: str = self.__ADDR_DEFAULT) -> None:
+    def __init__(self, name: str = __NAME_DEFAULT, num: str = __NUM_DEFAULT, addr: str = __ADDR_DEFAULT) -> None:
+        """
+        Is ran when an instance of the class is created.
+        Initialises 'name', 'num' and 'addr' to become attributes of the class.
+
+        :param str name: The contacts name.
+        :param str num: The contacts phone number.
+        :param str addr: The contacts address.
+
+        :return: None
+        """
+
         self.name: str = name
         self.num: str = num
         self.addr: str = addr
 
     def __del__(self) -> None:
+        """
+        Is ran when an instance of the class is deleted.
+
+        :return: None
+        """
+
         print(f"Contact ({self.name}) deleted... ")
 
     def __str__(self) -> str:
+        """
+        Returns information about the class.
+
+        :return: str
+        """
+
         return f"NAME: {self.name}" \
             f"NUMBER: {self.num}" \
             f"ADDRESS: {self.addr}"
 
     def __repr__(self) -> str:
+        """
+        Returns the code required to reproduce the class
+
+        :return: str
+        """
+
         return f"Contact('{self.name}', '{self.num}', '{self.addr}')"
--- main.py
+++ main.py
@@ -4,34 +4,101 @@ from json import loads, dumps
 from pathlib import Path
 from sys import exit as die
 from re import findall
+from time import sleep
+from typing import Union, Generator
+from asyncio import run as async_run
 
 
 class Main:
+    """
+    ATTRIBUTES
+    ----------------
+    PRIVATE | __CONTACTS_DIR : str
+    PRIVATE | __MENU_ITEMS : tuple[str, ...]
+    PRIVATE | __NUMBER_REGEX: str
 
-    __CONTACTS_DIR: str = "./contacts.json"
+    METHODS
+    ---------------
+    PRIVATE | __new__ -> object
+    PRIVATE | __init__ -> None
+    PRIVATE | __str__ -> str
 
+    PUBLIC | run -> None
+    PRIVATE | _menu -> None
+    PRIVATE | _display -> None
+    PRIVATE | _create -> None
+    PRIVATE | _delete -> None
+    PRIVATE | _save -> None
+    PRIVATE | _load -> list or dict
+    PUBLIC | read_json -> list or dict
+    PUBLIC | write_file -> None
+    """
+
+    # file directories
+    __CONTACTS_DIR: str = r"./contacts.json"
+
+    # tuples
     __MENU_ITEMS: tuple[str, ...] = ("View Contacts", "New Contact", "Delete Contact")
 
+    # regex's
     __NUMBER_REGEX: str = r"^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$"
 
     def __new__(cls) -> object:
+        """
+        Is ran when an instance of the class is created.
+
+        :return: object
+        """
+
         return object.__new__(cls)
 
     def __init__(self) -> None:
-        self._contacts: list[Contact] = list(self._load())
+        """
+        Is ran when an instance of the class is created.
+        Initialises a list of all saved contacts.
+
+        :return: None
+        """
+
+        self._contacts: list[Contact] = list(self._load())  # A list of all saved contacts
 
     def __str__(self) -> str:
+        """
+        Returns information about the class.
+
+        :return: str
+        """
+
         return f"Main class. Reading from: {self.__CONTACTS_DIR}"
 
     def run(self) -> None:
+        """
+        Is ran to start program.
+
+        :return: None
+        """
+
         self._menu()
 
     def _menu(self) -> None:
+        """
+        Displays a menu for the user to select what they would like to do.
+
+        :return: None
+        """
+
         while 1:
+            sleep(1)
             print("\nPlease enter the number of the function you would like to execute: ")
-            for index, item in enumerate(self.__MENU_ITEMS):
+            for index, item in enumerate(self.__MENU_ITEMS):  # displays all menu items
                 print(f"\t[{index+1}] {item}")
+            sleep(1)
+
             try:
+                """
+                Checks if user entered a valid number,
+                Then runs correct function or displays error message.
+                """
                 match int(input()):
                     case 1:
                         self._display()
@@ -41,24 +108,36 @@ class Main:
                         self._delete()
                     case _:
                         print("ERROR: Invalid function number. Please try again...")
-            except ValueError:
+            except ValueError:  # if user didn't enter a number
                 print("ERROR: Invalid function number. Please try again...")
 
     def _display(self) -> None:
-        if self._contacts:
+        """
+        Displays a list of saved contacts.
+
+        :return: None
+        """
+
+        if self._contacts:  # if the list is not empty
             print("\nCONTACTS:")
-            for index, item in enumerate(self._contacts):
+            for index, item in enumerate(self._contacts):  # display contacts
                 print(f"\tName: {item.name}\n\tNumber: {item.num}\n\tAddress: {item.addr}")
                 print() if index >= 0 else None
         else:
             print("ERROR: No contacts exist. Create a contact...")
 
     def _create(self) -> None:
+        """
+        Displays menu for user to create new contact.
+
+        :return: None
+        """
+
         print("\nNEW CONTACT:")
 
-        while 1:
+        while 1:  # get input for 'name' until valid input is given
             name: str = input("\tEnter name of contact: ").title()
-            if not name:
+            if not name:  # check if 'name' is empty
                 print("\n\tERROR: Name is empty. Please try again...\n")
             else:
                 break
@@ -67,10 +146,10 @@ class Main:
             num: str = input("\tEnter number of contact: ")
             if not num:
                 print("\n\tERROR: Contact number is empty. Please try again...\n")
-            elif findall(self.__NUMBER_REGEX, num):
-                break
-            else:
+            elif not findall(self.__NUMBER_REGEX, num):  # match against regex to check for correct formatting
                 print("\n\tERROR: Contact number is not in correct format (correct formats are '07XXXXXXXXX' or '+447XXXXXXXXX'). Please try again...\n")
+            else:
+                break
 
         while 1:
             addr: str = input("\tEnter address of contact: ").title()
@@ -78,45 +157,88 @@ class Main:
                 print("\n\tERROR: Address is empty. Please try again...\n")
             else:
                 break
-
-        contact: Contact = Contact(name, num, addr)
         
-        self._contacts.append(contact)
-        self._save()
+        self._contacts.append(Contact(name, num, addr))  # append new instance of class 'Contact' to list
+        async_run(self._save())  # save to file
 
     def _delete(self) -> None:
+        """
+        Displays menu for user to choose which contact to delete.
+
+        :return: None
+        """
+
         print("\nEnter the number of the contact you would like to delete:")
-        for index, item in enumerate(self._contacts):
+        for index, item in enumerate(self._contacts):  # display name of all contacts
             print(f"\t[{index+1}] {item.name}")
+
         try:
             index = int(input())-1
             if index <= len(self._contacts):
-                self._contacts.pop(index)
-                self._save()
+                self._contacts.pop(index)  # remove contact from list
+                async_run(self._save())  # save to file
             else:
                 print("ERROR: Invalid contact number. Please try again...")
 
-        except ValueError:
+        except ValueError:  # if a number isn't entered
             print("ERROR: Invalid contact number. Please try again...")
 
-    def _save(self) -> None:
-        self._write_file(self.__CONTACTS_DIR, dumps(list(repr(self._contacts[index]) for index, _ in enumerate(self._contacts)), sort_keys=True, indent=4))
+    async def _save(self) -> None:
+        """
+        Saves contacts to savefile.
+        Asynchronous.
 
-    def _load(self) -> iterator[Contact]:
-        for item in self._read_json(self.__CONTACTS_DIR):
-            yield eval(item)
+        :return: None
+        """
+
+        async_run(self.write_file(self.__CONTACTS_DIR, (repr(self._contacts[index]) for index, _ in enumerate(self._contacts))))
+
+    def _load(self) -> Generator[Contact]:
+        """
+        Loads the contents of the savefile.
+
+        :return: Generator[Contact]
+        """
+
+        for item in self.read_json(self.__CONTACTS_DIR):  # loop through savefile
+            yield eval(item)  # return generator containing instances of 'Contact'
 
     @classmethod
-    def _read_json(cls, path: str) -> list or dict:
-        return loads(open(path, "r").read()) if Path(path).is_file() else die("ERROR: File '{path}', does not exist...")
+    def read_json(cls, path: str) -> Union[list, dict]:
+        """
+        Returns JSON data from a specified file if the file exists.
+        Else exits program and displays error.
+
+        :param str path: The directory to read the JSON data from.
+
+        :return: Union[list, dict]
+        """
+
+        return loads(open(path, "r").read()) if Path(path).is_file() else die("FATAL ERROR: File '{path}', does not exist...")
 
     @classmethod
-    def _write_file(cls, path: str, data: str) -> None:
-        with open(path, "w") as fn:
-            fn.write(data)
+    async def write_file(cls, path: str, data: Union[list, dict, Generator]) -> None:
+        """
+        Writes JSON data to a specified file.
+        Asynchronous.
+
+        :param str path: The directory to write the file to.
+        :param str data: The data to write to the file.
+
+        :return: None
+        """
+
+        open(path, "w").write(dumps(data, sort_keys=True, indent=4))
 
 
 def run() -> None:
+    """
+    Create instance of 'Main' class.
+    Run 'Main' class.
+
+    :return: None
+    """
+
     main: Main = Main()
     main.run()
 
